5cf665cae4e3c1d5339f144a470243c63de013db
Deepak Srinath <deepak.s.nalore@gmail.com>
2020-04-11 01:33:46 -0500

    Updated client - parallelized

--- Implementation/client.py
+++ Implementation/client.py
@@ -1,19 +1,42 @@
+from gevent import monkey; monkey.patch_all()
 import requests
-import csv
 import json
-import time
+import random
+
+from gevent.pool import Pool
+
+p = Pool(10)
+
+# clemson   utah
+CLUSTER = "wisconsin"
 
 CLIENT_ID = "client_1"
-SERVER_LIST = ["http://127.0.0.1:5000/"]
-payload = {'client_id': CLIENT_ID, 'info': ''}
+SERVER_LIST = ["http://127.0.0.1:5000/", "http://127.0.0.1:5000/"]
+payload = {'client_id': CLIENT_ID, 'cluster': CLUSTER}
 
 headers = {
   'Content-Type': 'application/json'
 }
 
-payload['info'] = 'hello'
-for server in SERVER_LIST:
+def fetch(server):
     response = requests.request("POST", server + "request", headers=headers, data=json.dumps(payload))
     data = response.json()
     print(data)
+
+
+#initialize
+# for server in SERVER_LIST:
+#     payload['info'] = random.randrange(1, 1000)
+#     fetch(server)
+
+requests.request("GET", server + 'filewrite')
+
+for i in range(100):
+    payload['info'] = random.randrange(1, 1000)
+    for server in SERVER_LIST:
+        p.spawn(fetch,server)
+    p.join()
+
+for server in SERVER_LIST:
+    requests.request("GET", server + 'filewrite')
 
